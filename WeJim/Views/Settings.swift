deff0e33785030f2f20fad57d889f421367f09e3
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-07-07 19:07:53 -0500

    Added settings sheet. Commented out buggy color and rounded font modifiers.

---
+++ WeJim/Views/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  WeJim
+//
+//  Created by Aiden Barnard on 7/7/24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
