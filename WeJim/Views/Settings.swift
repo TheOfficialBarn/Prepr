d5fa6c2607d35416a313de2e2f6173f4370619c6
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-07-07 19:41:10 -0500

    Settings is fixed.

--- WeJim/Views/Settings.swift
+++ WeJim/Views/Settings.swift
@@ -7,9 +7,18 @@
 
 import SwiftUI
 
+
 struct Settings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Text("Hello")
+                Text("These are your settings")
+                Text("To be implemented later on")
+            }
+            .navigationTitle("Settings")
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
